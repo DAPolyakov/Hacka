04e858ef5fc6fca948413eedd32e1e396665d67a
dima <dp@redstart.pro>
2018-07-20 23:20:18 +0300

    init commit

---
+++ app/src/main/java/ru/yandexschool/hackathon/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.yandexschool.hackathon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
