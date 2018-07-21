dcb14b2a6a7330c46d13b10eb7befd196ab7b4c5
dima <dp@redstart.pro>
2018-07-21 16:14:14 +0300

    add finish game

--- app/src/main/java/ru/yandexschool/hackathon/ui/main/MainActivity.kt
+++ app/src/main/java/ru/yandexschool/hackathon/ui/main/MainActivity.kt
@@ -14,43 +14,50 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-    val random = Random(System.currentTimeMillis())
+    var gameIsFinished = false
 
-    private var speed = 3000L
+    val random = Random(System.currentTimeMillis())
+    private var speed = 2000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bagFieldRv.layoutManager = GridLayoutManager(this, 4)
-        bagFieldRv.adapter = BagRvAdapter()
+        bagFieldRv.adapter = BagRvAdapter(object : BagRvListener {
+            override fun finishGame() {
+                this@MainActivity.finishGame()
+            }
+        })
 
         startBugTimer()
     }
 
+    private fun finishGame() {
+        gameIsFinished = true
+    }
 
     private fun startBugTimer() {
-        Observable
-                .just(1)
-                .delay(speed, TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    Log.wtf("bugbug", "new")
-                    speed -= 300
-                    speed = Math.max(speed, 300L)
+        if (!gameIsFinished) {
+            Observable
+                    .just(1)
+                    .delay(speed, TimeUnit.MILLISECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        Log.wtf("bugbug", "new")
+                        speed -= 300
+                        speed = Math.max(speed, 300L)
 
-                    val r = Math.abs(random.nextInt() % 100)
+                        val r = Math.abs(random.nextInt() % 100)
 
-                    when {
-                        
+                        val item = if (r < 80) SmallBag() else EvilBag()
+
+                        (bagFieldRv?.adapter as? BagRvAdapter)?.addItemToField(item)
+
+                        startBugTimer()
                     }
-
-                    (bagFieldRv?.adapter as? BagRvAdapter)?.addItemToField(SmallBag())
-
-                    startBugTimer()
-                }
-
+        }
     }
 
 }
